2559190d48fe37d253ac91605fe8f8805acaa477
Jimpa <jamtshojimpa@gmail.com>
2025-03-24 17:41:50 +0600

    Update user data api

--- app/controllers/concerns/authenticable.rb
+++ app/controllers/concerns/authenticable.rb
@@ -10,11 +10,15 @@ module Authenticable
 
   def authenticate_request
     header = request.headers["Authorization"]
+
+    return render json: { error: "Missing token" }, status: :unauthorized if header.nil?
     token = header.split(" ").last if header
     # binding.pry
     begin
       decoded = JWT.decode(token, Rails.application.credentials.secret_key_base)
       @current_user = User.find(decoded[0]["user_id"])
+    rescue JWT::ExpiredSignature
+      render json: { error: "Expired token" }, status: :unauthorized
     rescue JWT::DecodeError
       render json: { error: "unauthorized" }, status: :unauthorized
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,22 @@
 class User < ApplicationRecord
   has_secure_password
   validates :username, presence: true, uniqueness: true
+  # validate :dob_cannot_be_in_future
+  enum :gender, {
+    Male: 0,
+    Female: 1
+  }
+
+  ADMIN_ROLE_ID = 0
+  USER_ROLE_ID = 1
+
+  def admin?
+    role_id == ADMIN_ROLE_ID
+  end
+
+  def dob_cannot_be_in_future
+    if dob.present? && dob > Date.today
+      errors.add(:dob, "Can't be in future")
+    end
+  end
 end
